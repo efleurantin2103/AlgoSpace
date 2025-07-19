0ee935a9448ff906048701acbb1b805dd6e93c80
Emmanuel Fleurantin <67471721+efleurantin2103@users.noreply.github.com>
2025-07-19 14:46:54 -0400

    Update main.m

--- Scalar_Paper_3cases/scalar_e2/main.m
+++ Scalar_Paper_3cases/scalar_e2/main.m
@@ -7,7 +7,7 @@ c=-20;
 mu=linspace(0.05,0.25,10 ...
     );
 
-%% BEGIN COMPUTE POTENTIAL for sigma system
+%% BEGIN COMPUTE INNER POTENTIAL for sigma system
 r = linspace(0,1000,238); %Create r vector from 0 to 1000 
 u = -((0.4)./((1+2*r).^2)).^2;% Calculate u
 
