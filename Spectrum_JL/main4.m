8d3ee6120bf96cec483a812602a210c3a20d1af4
Emmanuel Fleurantin <67471721+efleurantin2103@users.noreply.github.com>
2025-10-17 23:15:16 -0400

    Clean up threshold calculations and plotting
    Removed unused threshold calculations and plotting code.

--- Spectrum_JL/main4.m
+++ Spectrum_JL/main4.m
@@ -11,9 +11,6 @@ lambda = linspace(0.83,1,20);
 alpha=0.75;
 threshold = eps^(alpha);
 threshold2 = 1-eps^(alpha);
-% kappa=0.65;%works fine, relationship to alpha
-% threshold = eps^(kappa)/(1+eps^(kappa));
-% threshold2 = eps^(kappa-1)/(1+eps^(kappa-1));
 
 %%%%%%%%%%%%%%%%%%%%%%%%
 %% BEGIN COMPUTE SOLITON
@@ -68,11 +65,6 @@ for i=1:nmax
     options = bvpset('SingularTerm',S,'FJacobian',solit_jac_handle,'BCJacobian',solit_bc_jac_handle);    
     sol = bvp4c(solit_sys_handle, solit_bc_handle, sol, options);
 end
-
-%% Plot soliton
-%
-% plot(sol.x, sol.y(1,:));
-% axis([0 10 0 inf]);
 
 %% Vectorize the soliton so that it may be made into a function.
 %
@@ -227,6 +219,3 @@ ax.XAxis.TickLength = [0.015 0.015];
 ax.YAxis.TickLength = [0.015 0.015];
 set(gcf, 'Color', 'w')
 end
-
-
-print(1, '-djpeg', '-r300', 'JL_combined2.jpg')
