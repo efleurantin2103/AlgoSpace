d47e7cc387d589c5972a8d066a82969f4d20b785
Emmanuel Fleurantin <67471721+efleurantin2103@users.noreply.github.com>
2025-11-18 10:52:52 -0500

    Refactor plucker_guess function for clarity
    Removed unnecessary comments and cleaned up formatting.

--- Spectrum_JL/plucker_guess.m
+++ Spectrum_JL/plucker_guess.m
@@ -8,6 +8,6 @@ f(3,1) = -(x/(1+x^2)^1.5);
 f(4,1) = -(x/(1+x^2)^1.5);
 f(5,1) = -(x/(1+x^2)^1.5);
 f(6,1) = -(x/(1+x^2)^1.5); %added this extra line here for the 
-                           %compatified system. Not sure if this the right
-                           %one
+                           %compatified system.
 end
+
